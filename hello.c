5cbb97cac54fc238515a6d0ffb52b2a058266558
Luca Sturla <101606349+luca345@users.noreply.github.com>
2022-03-28 16:56:07 -0300

    Delete hello.c

--- hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main (void){
-    printf ("Hola, cual es tu nombre?\n");
-    char nombre [20];
-    scanf("%s",&nombre);
-    printf ("Bienvenido %s\n",nombre);
-    return 0;
-}
